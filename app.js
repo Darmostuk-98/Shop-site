99d5027aa42158b18581cb51dafecd71d52e2d33
darmostuk98 <peterdarmostuk@gmail.com>
2023-01-12 13:34:54 +0200

    delete some code from js

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ for (let i = 0; i < addToCartButton.length; i++) {
   });
 }
 
-let likeBtn = document.getElementById("like-toggle");
-
-function likeToggle() {
-  if (likeBtn.style.backgroundColor == "red") {
-    likeBtn.style.backgroundColor == "green";
-  } else {
-    likeBtn.style.backgroundColor == "red";
-  }
-}
-
 function likeToggle() {
   document.getElementById("like-toggle").classList.toggle("active");
 }
